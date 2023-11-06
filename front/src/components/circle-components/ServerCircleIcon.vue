c6b59aa498bc26c5c9d3a715737cfccf82366663
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2023-11-06 22:45:04 +0100

    feature/adding circleIcon components, WIP

---
+++ front/src/components/circle-components/ServerCircleIcon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+    <div class="flex w-full justify-center relative mb-2">
+        <div class="absolute h-[48px] w-2 top-0 left-0 flex items-center justify-start ">
+            <div class="h-[40px] w-full bg-white-600 block rounded-r-[4px] -ml-[4px]"></div>
+        </div>
+        <div class="w-[48px] h-[48px] flex items-center justify-center cursor-pointer bg-blue rounded-2xl">
+            <img class="w-3/5" src="../../assets/discord-mark-white.svg" >
+        </div>
+    </div>  
+
+</template>
+
+<style scoped>
+
+</style>
