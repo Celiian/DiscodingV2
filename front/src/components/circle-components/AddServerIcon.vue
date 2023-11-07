3d1784910d40fd745bcc66ca047571947f65c336
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2023-11-07 15:21:41 +0100

    fix/closing modal when click outside

--- front/src/components/circle-components/AddServerIcon.vue
+++ front/src/components/circle-components/AddServerIcon.vue
@@ -23,7 +23,7 @@ function closeModal(){
         <div class="group">
             <!-- discord icon -->
             <div class=" w-[48px] h-[48px] flex items-center justify-center cursor-pointer bg-grey-400  group-hover:bg-green-circleIcon rounded-[50%] group-hover:rounded-2xl transition-all duration-300">
-                <PlusIcon :onclick="openModal" class="h-1/3 fill-green-circleIcon group-hover:fill-white-600 transition-all duration-300"/>
+                <PlusIcon @click="openModal" class="h-1/3 fill-green-circleIcon group-hover:fill-white-600 transition-all duration-300"/>
             </div>
           
         </div>
--- front/src/components/modal/CreateServerModal.vue
+++ front/src/components/modal/CreateServerModal.vue
@@ -1,22 +1,31 @@
 <script setup lang="ts">
 import CloseIcon from '../svg/CloseIcon.vue';
 import AddPhotoIcon from '../svg/AddPhotoIcon.vue';
+import { useUserStore } from '../../store/userstore';
+import { onClickOutside } from '@vueuse/core'
+import { ref } from 'vue';
 
 const emit = defineEmits(["close-modal"]);
+const userStore = useUserStore();
+
+const currentUser = "titi"//userStore.currentUser
+console.log(userStore.currentUser)
+
+const target = ref(null)
 
 
 //METHOD
 function closeModal(){
     emit("close-modal")
 }
-
+onClickOutside(target, ()=> closeModal())
 </script>
 
 <template>
     <div class="absolute top-0 left-0 w-screen h-screen bg-black/70 z-10 flex justify-center items-center">
-        <div class="bg-white-600 rounded min-w-[440px] sm:w-[440px] w-full h-full sm:h-auto relative">
+        <div ref="target" class="bg-white-600 rounded min-w-[440px] sm:w-[440px] w-full h-full sm:h-auto relative">
 
-            <button :onclick="closeModal" class="absolute top-4 right-4 w-6 h-6">
+            <button @click="closeModal" class="absolute top-4 right-4 w-6 h-6">
                 <CloseIcon class="w-full h-full fill-white-200 hover:fill-black transition-all duration-300"/>  
             </button>
 
@@ -26,13 +35,15 @@ function closeModal(){
             </div>
 
             <div class="my-4 flex flex-col px-4">
-                <button class="flex justify-center pt-1">
-                    <AddPhotoIcon/>
-                </button>
+                <div class="flex justify-center pt-1">
+                    <button>
+                        <AddPhotoIcon/>
+                    </button>    
+                </div>
 
                 <div class="w-full flex flex-col mt-6">
                     <h3 class="mb-2 text-xs font-bold text-grey-500">NOM DU SERVEUR</h3>
-                    <input type="text" placeholder="Serveur de titi" class="w-full h-[40px] bg-white-500 rounded p-[10px] placeholder:text-grey-300 focus:outline-0">
+                    <input type="text" :placeholder="'Serveur de ' + currentUser " class="w-full h-[40px] bg-white-500 rounded p-[10px] placeholder:text-grey-300 focus:outline-0">
                     <p class="mt-2 pb-1 text-[#5c5e66] text-xs font-normal">En créant un serveur, tu acceptes la <a href="" class="font-semibold text-[#00a8fc]">Charte d'Utilisation de mon c*l de Discord</a>.</p>
                 </div>
                 
