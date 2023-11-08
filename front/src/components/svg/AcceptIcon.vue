16beb1025b4c3cfb770ab1bbcdbfbeaf75d86c82
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2023-11-08 15:19:31 +0100

    wip/adding two more svg for friendcard list

---
+++ front/src/components/svg/AcceptIcon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+<svg role="img" width="24" height="24" viewBox="0 0 24 24">
+    <path d="M8.99991 16.17L4.82991 12L3.40991 13.41L8.99991 19L20.9999 7.00003L19.5899 5.59003L8.99991 16.17Z"></path>
+</svg>
+
+</template>
+
+<style scoped>
+
+</style>
