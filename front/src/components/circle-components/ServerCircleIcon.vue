80d2dac01964b3faf445c00e80cd5fbeb0799d1e
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2023-11-09 14:13:44 +0100

    fix/server icon image ratio

--- front/src/components/circle-components/ServerCircleIcon.vue
+++ front/src/components/circle-components/ServerCircleIcon.vue
@@ -8,14 +8,12 @@ const { name, imageUrl, id } = defineProps(["name", "imageUrl", "id"]);
 <template>
   <router-link class="flex w-full justify-center relative mb-2 overflow-x-clip" :to="'/server/' + id">
     <div class="group">
-      <img
-        :src="imageUrl"
-        class="w-[48px] h-[48px] flex items-center justify-center cursor-pointer rounded-[50%] group-hover:rounded-2xl transition-all duration-300"
-      />
+      <img :src="imageUrl"
+        class="w-[48px] h-[48px] flex items-center justify-center cursor-pointer rounded-[50%] group-hover:rounded-2xl transition-all duration-300 object-cover" />
       <div class="absolute h-[48px] w-2 top-0 left-0 flex items-center justify-start">
         <div
-          class="h-[8px] group-hover:h-[20px] w-full bg-white-600 block rounded-r-[4px] -ml-[4px] transition-all duration-300"
-        ></div>
+          class="h-[8px] group-hover:h-[20px] w-full bg-white-600 block rounded-r-[4px] -ml-[4px] transition-all duration-300">
+        </div>
       </div>
 
       <!-- group-hover:block hidden  -->
