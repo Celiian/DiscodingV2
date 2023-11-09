d821e797795cd1ebb6a19e9ea1d8c46af6c13b69
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2023-11-09 19:00:56 +0100

    feat/server async request to get channel and category

--- front/src/components/server/ServerDetailNavContent.vue
+++ front/src/components/server/ServerDetailNavContent.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
+import { computed, onMounted } from 'vue';
 import ServerCaterogyCard from './ServerCaterogyCard.vue';
 import ServerChannelNameCard from './ServerChannelNameCard.vue';
+import { useRoute } from 'vue-router';
+import { useServerStore } from '../../store/serverstore';
+
+const serverStore = useServerStore()
+
+onMounted(async () => {
+    const test = await serverStore.getChannelsServer(serverId.value)
+    console.log(test.channels.length)
+})
+
+const routes = useRoute();
+
+const serverId = computed(() => {
+    return routes.params.serverId as string;
+});
+
 
 
 const aa = ["channel1", "channel2", "channel3"]
