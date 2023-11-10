02a35ecfe9ce70948457c04c2d06581317cb223c
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2023-11-10 08:08:10 +0100

    fix/z index for dropdown menu

--- front/src/components/home/DetailNav.vue
+++ front/src/components/home/DetailNav.vue
@@ -26,7 +26,8 @@ function openSettingModal() {
 </script>
 
 <template>
-  <div class="main -mt-0 w-[240px] h-full bg-grey-300 flex flex-col grow-0 shrink-0 basis-auto overflow-y-scroll overflow-x-hidden relative ">
+  <div
+    class="main z-50 -mt-0 w-[240px] h-full bg-grey-300 flex flex-col grow-0 shrink-0 basis-auto overflow-y-scroll overflow-x-hidden relative ">
     <nav class="flex flex-col flex-1 ">
       <div class="flex grow-0 shrink-0 basis-auto px-2 h-12 items-center shadow-customOne fixed bg-grey-300 w-[240px]">
         <slot name="header">
@@ -40,8 +41,7 @@ function openSettingModal() {
     </nav>
 
     <div
-      class="h-[52px] min-h-[52px] bg-grey-200 flex grow-0 shrink-0 basis-auto w-[240px] fixed bottom-0 px-2 items-center font-medium text-[14px] justify-between"
-    >
+      class="h-[52px] min-h-[52px] bg-grey-200 flex grow-0 shrink-0 basis-auto w-[240px] fixed bottom-0 px-2 items-center font-medium text-[14px] justify-between">
       <div class="group flex items-center -ml-[2px] min-w-[120px] pl-[2px] mr-2 h-[80%] hover:bg-white-100/20 rounded">
         <div class="w-8 h-8">
           <RoundedLogoIcon :icon="currentUser?.icon" />
@@ -51,23 +51,19 @@ function openSettingModal() {
           <div class="text-white-300 text-[13px] font-medium">{{ currentUser?.username }}</div>
           <div class="relative text-white-200 text-[12px] whitespace-nowrap">
             <span
-              class="absolute -top-1 group-hover:opacity-0 opacity-100 translate-y-0 group-hover:-translate-y-3 transition-all duration-150"
-            >
+              class="absolute -top-1 group-hover:opacity-0 opacity-100 translate-y-0 group-hover:-translate-y-3 transition-all duration-150">
               En ligne
             </span>
             <span
-              class="absolute -top-1 opacity-0 translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-150"
-            >
+              class="absolute -top-1 opacity-0 translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-150">
               {{ currentUser?.username }}{{ currentUser?.tag }}
             </span>
           </div>
         </div>
       </div>
       <div class="flex justify-end items-end pr-2">
-        <div
-          @click="openSettingModal"
-          class="p-1 rounded hover:bg-white-200/50 transition-all duration-150 cursor-pointer"
-        >
+        <div @click="openSettingModal"
+          class="p-1 rounded hover:bg-white-200/50 transition-all duration-150 cursor-pointer">
           <SettingIcon class="fill-white-300" />
         </div>
       </div>
